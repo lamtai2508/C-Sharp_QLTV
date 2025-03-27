73cb6404d0d10832e69b442be3946480f76beb9c
hồng tài <lamtai2508@gmail.com>
2025-03-28 00:08:01 +0700

    update UI MemberList

--- UI_For_Admin/MemberList.cs
+++ UI_For_Admin/MemberList.cs
@@ -21,11 +21,6 @@ namespace QLTV
             dbHelper = new DatabaseHelep();
             this.Dock = DockStyle.Fill;
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
         private void LoadData()
         {
             members_table.DataSource = UsersRepository.GetUser();
@@ -39,41 +34,15 @@ namespace QLTV
         private void label2_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            if (member_id_tb.Text == "")
-            {
-                member_id_tb.Text = "Mã thành viên";
-                member_id_tb.ForeColor = Color.Black;
-            }
-        }
-        private void textBox2_Enter(object sender, EventArgs e)
-        {
-            if (member_id_tb.Text == "Mã thành viên")
-            {
-                member_id_tb.Text = "";
-                member_id_tb.ForeColor = Color.Black;
-            }
-        }
-
-        private void textBox2_Leave(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(member_id_tb.Text))
-            {
-                member_id_tb.Text = "Mã thành viên";
-                member_id_tb.ForeColor = Color.Gray;
-            }
-        }
-
-        private void Form3_Load(object sender, EventArgs e)
+        } 
+        private void MemberList_Load(object sender, EventArgs e)
         {
             LoadData();
-            member_id_tb.Text = "Mã thành viên";
-            member_id_tb.ForeColor = Color.Gray;
-            member_id_tb.Enter += textBox2_Enter;
-            member_id_tb.Leave += textBox2_Leave;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
