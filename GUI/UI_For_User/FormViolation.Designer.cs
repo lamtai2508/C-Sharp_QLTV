7c27218fb3033d75df9e40b7d9e7bd4c1f9e8e97
4honestobs <huynhtien180103@gmail.com>
2025-03-29 20:55:24 +0700

    ADD Reserve form, Violation History form to User GUI

---
+++ GUI/UI_For_User/FormViolation.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace qltv.GUI.UI_For_User
+{
+    partial class FormViolation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormViolation));
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label2 = new Label();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(0, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(286, 32);
+            label1.TabIndex = 0;
+            label1.Text = "LỊCH SỬ MƯỢN THIẾT BỊ";
+            label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 270);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1097, 378);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            flowLayoutPanel1.Location = new Point(143, 206);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(684, 39);
+            flowLayoutPanel1.TabIndex = 2;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label2.Location = new Point(150, 0);
+            label2.Margin = new Padding(150, 0, 3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 28);
+            label2.TabIndex = 0;
+            label2.Text = "Tìm kiếm: ";
+            label2.Click += label2_Click;
+            // 
+            // panel2
+            // 
+            panel2.AutoSize = true;
+            panel2.BackColor = SystemColors.ControlDark;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(267, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(340, 32);
+            panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Dock = DockStyle.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(308, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(30, 30);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(0, 0);
+            textBox1.Margin = new Padding(0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(676, 26);
+            textBox1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button4.AutoSize = true;
+            button4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(613, 3);
+            button4.Name = "button4";
+            button4.Padding = new Padding(10, 0, 10, 0);
+            button4.Size = new Size(68, 33);
+            button4.TabIndex = 3;
+            button4.Text = "Tìm";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // FormViolation
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1121, 751);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "FormViolation";
+            Text = "XemLichSu";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label2;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button4;
+    }
+}
