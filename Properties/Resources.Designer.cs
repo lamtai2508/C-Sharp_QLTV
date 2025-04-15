8fa8e79ff16310b370b2c67a03766c6437de7714
hồng tài <lamtai2508@gmail.com>
2025-04-15 14:22:53 +0700

    update web

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace qltv.Properties {
+namespace qltv_Winform.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace qltv.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("qltv.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("qltv_Winform.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
