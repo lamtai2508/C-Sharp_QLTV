8e6cee48176e1c7770a28928a80c0d4b0a217691
4honestobs <huynhtien180103@gmail.com>
2025-05-05 21:00:21 +0700

    resolve conflict when remove background.jpg from resouces folder

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -60,15 +60,6 @@ namespace qltv_Winform.Properties {
             }
         }
         
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap background {
-            get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
         
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
