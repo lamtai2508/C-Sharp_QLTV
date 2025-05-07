46a18a29a6a8f71d1dc7d76598899cab739cefdb
4honestobs <huynhtien180103@gmail.com>
2025-05-07 20:19:17 +0700

    temp khu vuc hoc tap

---
+++ DAO/PresentMemberDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using qltv.DTO;
+using QLTV.Resources;
+
+namespace qltv_Winform.DAO
+{
+    internal class PresentMemberDAO
+    {
+
+        // lấy tất cả dữ liệu của bảng presentmembers
+        public static DataTable GetALLPresentMember()
+        {
+            // Lấy tất cả trừ admin ra
+            string query = "select * from presentmembers";
+            return DatabaseHelper.GetData(query);
+        }
+
+        
+
+        // thêm thành viên vào bảng presentmembers
+        public static bool AddPresentMember(PresentMemberDTO presentMember)
+        {
+            string query = "insert into presentmembers(member_id, full_name, appear_time, leave_time) values(@member_id, @full_name, @appear_time, @leave_time)";
+
+            using (MySqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@member_id", presentMember.member_id);
+                    cmd.Parameters.AddWithValue("@full_name", presentMember.full_name);
+                    cmd.Parameters.AddWithValue("@appear_time", presentMember.appear_time);
+                    cmd.Parameters.AddWithValue("@leave_time", presentMember.leave_time);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Lỗi: " + ex.Message);
+                        return false;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DTO/PresentMemberDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qltv.DTO
+{
+    public class PresentMemberDTO
+    {
+        public string member_id { get; set; }
+        public string full_name { get; set; }
+        public DateTime appear_time { get; set; }
+        public DateTime leave_time { get; set; }
+
+        public PresentMemberDTO(string member_id, string full_name, DateTime appear_time, DateTime leave_time) { 
+            this.member_id = member_id;
+            this.full_name = full_name;
+            this.appear_time = appear_time;
+            this.leave_time = leave_time;
+        }
+
+    }
+}
